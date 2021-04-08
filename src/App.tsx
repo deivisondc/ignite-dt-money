cfe45df67b29cbdc4e671e932567a70cd2a0e168
Deivison Cardoso <deivisondc@gmail.com>
2021-04-07 23:24:01 -0300

    add mirage and transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyles } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTranscationModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTranscationModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTranscationModal} />
       <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTranscationModal}
+      />
 
       <GlobalStyles />
     </>
